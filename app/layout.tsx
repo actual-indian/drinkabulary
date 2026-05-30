5f2f24c90502ba77c5a7147cbe0d6d59f3f04e9e
actual-indian <simon@aardbeilabs.com>
2026-05-30 15:08:52 +0200

    re-added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/contexts/ThemeContext';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Valhalla Beer Club',
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
